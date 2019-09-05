1d4e20f048d8e71233c0ec442e591a4db02be44c
IliasKabden <ilias.kabden@gmail.com>
2019-09-05 11:57:35 +0600

    initial commit

---
+++ docs-core/src/main/java/com/sismics/docs/core/listener/async/FileDeletedAsyncListener.java
@@ -0,0 +1,46 @@
+package com.sismics.docs.core.listener.async;
+
+import com.google.common.eventbus.AllowConcurrentEvents;
+import com.google.common.eventbus.Subscribe;
+import com.sismics.docs.core.event.FileDeletedAsyncEvent;
+import com.sismics.docs.core.model.context.AppContext;
+import com.sismics.docs.core.model.jpa.File;
+import com.sismics.docs.core.util.FileUtil;
+import com.sismics.docs.core.util.TransactionUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Listener on file deleted.
+ * 
+ * @author bgamard
+ */
+public class FileDeletedAsyncListener {
+    /**
+     * Logger.
+     */
+    private static final Logger log = LoggerFactory.getLogger(FileDeletedAsyncListener.class);
+
+    /**
+     * File deleted.
+     * 
+     * @param event File deleted event
+     * @throws Exception e
+     */
+    @Subscribe
+    @AllowConcurrentEvents
+    public void on(final FileDeletedAsyncEvent event) throws Exception {
+        if (log.isInfoEnabled()) {
+            log.info("File deleted event: " + event.toString());
+        }
+
+        // Delete the file from storage
+        File file = event.getFile();
+        FileUtil.delete(file);
+
+        TransactionUtil.handle(() -> {
+            // Update index
+            AppContext.getInstance().getIndexingHandler().deleteDocument(file.getId());
+        });
+    }
+}
